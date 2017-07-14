3cc6f58f636fc61676b0bec361145b50d751f78f
roversmanoj <roversmanoj@gmail.com>
2017-07-14 18:15:56 +0530

    Updated greeter.java

--- multi-module/server/src/main/java/com/example/Greeter.java
+++ multi-module/server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,7 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
+    System.out.println("greeting");
     return String.format("Hello, %s!", someone);
   }
 }
